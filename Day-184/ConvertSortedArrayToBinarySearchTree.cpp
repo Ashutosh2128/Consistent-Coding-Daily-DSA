906aed2ea4dfc46238d08eb27ad96985db673f29
Ashutosh2128 <prustyashutosh2003@gmail.com>
2026-05-13 10:49:49 +0530

    Day-184 Completed

---
+++ Day-184/ConvertSortedArrayToBinarySearchTree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    TreeNode* solve(vector<int>& nums, int s, int e) {
+        if(s > e) return nullptr;
+
+        int mid = s + (e - s) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+
+        root -> left = solve(nums, s, mid-1);
+        root -> right = solve(nums, mid+1, e);
+
+        return root;
+    }
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return solve(nums, 0, nums.size() - 1);
+    }
+};
--- README.md
+++ README.md
@@ -425,3 +425,5 @@
 
 ### Day-183: Kth Smallest Element in a BST (https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/)
 ###          Lowest Common Ancestor of a Binary Search Tree (https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/)
+
+### Day-184: Convert Sorted Array to Binary Search Tree (https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/)
